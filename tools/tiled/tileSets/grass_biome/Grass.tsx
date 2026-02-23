d4faf7d777609e2d5fd1b6443a68df6b126d2b48
raphir <raphaelhirscher@gmail.com>
2026-02-23 21:26:27 +0100

    Added more aseprite files and screenshots. Added tiled folder.

---
+++ tools/tiled/tileSets/grass_biome/Grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Grass" tilewidth="16" tileheight="16" tilecount="32" columns="8">
+ <image source="../../../../assets/grass_biome_4x8_16x16.png" width="128" height="64"/>
+</tileset>
