af8faa9a8542e2f4ea11b61936a6b9a74396b438
raphir <raphaelhirscher@gmail.com>
2026-02-28 00:02:23 +0100

    Various sprite edits, removals and creations. Added tiled maps and tilesets.

---
+++ tools/tiled/tileSets/grass_biome/grass_tall_masked.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="flora_masked" tilewidth="16" tileheight="16" tilecount="36" columns="6">
+ <image source="../../../../assets/flora_masked.png" width="96" height="96"/>
+</tileset>
