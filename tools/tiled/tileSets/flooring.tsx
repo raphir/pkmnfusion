feb7081c6e712d8155cf8826ee98b9a646a0ce85
raphir <raphaelhirscher@gmail.com>
2026-02-28 00:36:42 +0100

    More aseprite clean up. Updated prototype map.

--- tools/tiled/tileSets/flooring.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="flooring" tilewidth="16" tileheight="16" tilecount="128" columns="16">
- <image source="../../../assets/grass_biome.png" width="256" height="128"/>
-</tileset>
