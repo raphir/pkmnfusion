9286bad3dbe379009ec045d921851d8ec3a4805c
Raphael Hirscher <raphael.hirscher@finnova.com>
2026-03-02 08:28:47 +0100

    Replace manual tile system with bevy_ecs_tiled
    Gutted hand-coded MapData/TileRegistry/spawn_tilemaps. Now loads
    prototype.tmx via TiledPlugin. Player is colored rectangle with
    free movement (no collision). Phase 1 of tiled integration.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 mod components;
 mod map;
 mod player;
-mod tiles;
 
 use bevy::prelude::*;
-use bevy_ecs_tilemap::prelude::*;
-
-use components::{AnimatedTile, TileStepEvent};
-use tiles::TileRegistryResource;
+use bevy_ecs_tiled::prelude::*;
 
 fn main() {
     App::new()
@@ -22,40 +18,22 @@ fn main() {
                 })
                 .set(ImagePlugin::default_nearest()),
         )
-        .add_plugins(TilemapPlugin)
-        .insert_resource(TileRegistryResource::default())
-        .add_message::<TileStepEvent>()
-        .add_systems(Startup, setup)
+        .add_plugins(TiledPlugin::default())
+        .add_systems(
+            Startup,
+            (map::setup_map, player::spawn_player, setup_camera),
+        )
         .add_systems(
             Update,
             (
                 player::player_movement,
-                player::camera_follow,
                 player::update_sprite_positions,
-                animate_tiles,
+                player::camera_follow,
             ),
         )
         .run();
 }
 
-fn animate_tiles(time: Res<Time>, mut query: Query<(&mut AnimatedTile, &mut TileTextureIndex)>) {
-    for (mut anim, mut texture_index) in query.iter_mut() {
-        anim.timer.tick(time.delta());
-
-        if anim.timer.just_finished() {
-            anim.current_frame = (anim.current_frame + 1) % anim.frames.len();
-            texture_index.0 = anim.frames[anim.current_frame];
-        }
-    }
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
-
-    // Setup map with tile ID system
-    map::setup_map(commands.reborrow(), asset_server.clone());
-
-    // Spawn player and objects
-    player::spawn_player(&mut commands, &asset_server);
-    player::spawn_example_objects(&mut commands, &asset_server);
 }
